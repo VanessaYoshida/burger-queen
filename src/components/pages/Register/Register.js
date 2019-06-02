b33f3cddf104ae5ba59f4d1db7081757d8e84a92
Vanessa <vanessayoshida92@gmail.com>
2019-06-02 18:22:24 -0300

    Alteraçao na estrutura das pastas, e feito funcionalidade para login e navbar

---
+++ src/components/pages/Register/Register.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Button from '../../ui/Button/Button'
+import firebase from '../../../util/config/firebaseConfig';
+import withFirebaseAuth from 'react-with-firebase-auth';
+import PasswordMask from 'react-password-mask';
+
+const firebaseAppAuth = firebase.auth();
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+  
+  handleChange = (event, element) => {
+    const newState = this.state;
+    newState[element] = event.target.value
+    this.setState(newState);
+  }
+  
+  createUser = () => {
+    this.props.createUserWithEmailAndPassword(this.state.email, this.state.password);
+  }
+  
+  render() {
+    return (
+      <div>
+      <input value={this.state.email}
+      placeholder="E-mail"
+      onChange={(e) => this.handleChange(e, "email")} />
+      <PasswordMask
+        id="password"
+        name="password"
+        placeholder="Senha"
+        value={this.state.password}
+        onChange={(e) => this.handleChange(e, "password")}
+      />
+      <Button text="criar usuário" onClick={this.createUser} />
+      <Button text="loga com usuario" onClick={this.signIn} />
+      </div>
+      )
+    }
+  }
+  
+  export default withFirebaseAuth({
+    firebaseAppAuth,
+  })(Login);
