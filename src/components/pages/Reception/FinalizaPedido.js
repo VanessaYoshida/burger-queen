b0a8afd07b0ad0f401cb01e456009561c21817bc
Vanessa <vanessayoshida92@gmail.com>
2019-06-16 09:11:15 -0300

    Funçao map adicionada para mostrar cada item

--- src/components/pages/Reception/FinalizaPedido.js
+++ src/components/pages/Reception/FinalizaPedido.js
@@ -1,31 +1,6 @@
 import React, {Component} from 'react';
 import CarrinhoCompra from 'components/ui/CarrinhoCompra';
 
-// const menu = {
-//   "breakfast": [
-//       {item:"Café Americano", price: 5},
-//       {item:"Café com Leite", price: 7},
-//       {item:"Sanduíche de Presunto e Queijo", price: 10},
-//       {item:"Suco de Fruta Natural", price: 7}
-//   ],
-//   "lunch": [
-//       {item:"Hamburguer Simples Bovino", price:10 },
-//       {item:"Hamburguer Simples de Frango", price:10 },
-//       {item:"Hamburguer Simples Vegetariano", price:10 },
-//       {item:"Hamburguer Duplo Bovino", price:15 },
-//       {item:"Hamburguer Duplo Frango", price:15 },
-//       {item:"Hamburguer Duplo Vegetariano", price:15 },
-//       {item:"Queijo", price:1 },
-//       {item:"Ovo", price:1 },
-//       {item:"Batata Frita", price:5 },
-//       {item:"Anéis de Cebola", price:5 },
-//       {item:"Água 500ml", price:5 },
-//       {item:"Água 750ml", price:7 },
-//       {item:"Bebida gaseificada 500ml", price:7 },
-//       {item:"Bebida gaseificada 750ml", price:10 }
-//   ]
-// }
-
 class FinalizaPedido extends Component {
   
   render() {
--- src/components/ui/CarrinhoCompra.js
+++ src/components/ui/CarrinhoCompra.js
@@ -1,5 +1,30 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import ItemCarrinho from './ItemCarrinho';
+
+const menu = {
+  "breakfast": [
+      {item:"Café Americano", price: 5},
+      {item:"Café com Leite", price: 7},
+      {item:"Sanduíche de Presunto e Queijo", price: 10},
+      {item:"Suco de Fruta Natural", price: 7}
+  ],
+  "lunch": [
+      {item:"Hamburguer Simples Bovino", price:10 },
+      {item:"Hamburguer Simples de Frango", price:10 },
+      {item:"Hamburguer Simples Vegetariano", price:10 },
+      {item:"Hamburguer Duplo Bovino", price:15 },
+      {item:"Hamburguer Duplo Frango", price:15 },
+      {item:"Hamburguer Duplo Vegetariano", price:15 },
+      {item:"Queijo", price:1 },
+      {item:"Ovo", price:1 },
+      {item:"Batata Frita", price:5 },
+      {item:"Anéis de Cebola", price:5 },
+      {item:"Água 500ml", price:5 },
+      {item:"Água 750ml", price:7 },
+      {item:"Bebida gaseificada 500ml", price:7 },
+      {item:"Bebida gaseificada 750ml", price:10 }
+  ]
+}
 
 class CarrinhoCompra extends Component {
   constructor(props) {
@@ -12,8 +37,8 @@ class CarrinhoCompra extends Component {
 
   atualizaTotal = (total, id) => {
     
-    //pega todos os itens menos o atual
-    let itensTotal = this.state.itensTotal.filter((item, index) => index !== id);
+    //pega todos os itens
+    let itensTotal = this.state.itensTotal;
     //atualiza o valor do item atual
     itensTotal[id] = total;
   
@@ -25,8 +50,9 @@ class CarrinhoCompra extends Component {
   }
 
   render() {
+    const categorySelect = (this.state.category === "lunch") ? "lunch" : "breakfast";
     return (
-      <>
+      <Fragment>
       <table border="1">
         <thead>
         <tr>
@@ -37,18 +63,20 @@ class CarrinhoCompra extends Component {
         </tr>
         </thead>
         <tbody>
-        <ItemCarrinho id="0" produto="Café com leite" quantidade="0" valor="5" getTotal={this.atualizaTotal} ></ItemCarrinho>
-        <ItemCarrinho id="1" produto="Café puro" quantidade="0" valor="10" getTotal={this.atualizaTotal} ></ItemCarrinho>
-        <ItemCarrinho id="2" produto="Pão com manteiga" quantidade="0" valor="7" getTotal={this.atualizaTotal} ></ItemCarrinho>
-        <ItemCarrinho id="3" produto="coxinha de frango" quantidade="0" valor="2" getTotal={this.atualizaTotal}></ItemCarrinho>
-        <tr>
-          <td colSpan="4">
-            Total = {this.state.totalFinal}
-          </td>
-        </tr>
+          {
+            menu[categorySelect].map((product, i) => {
+              return <ItemCarrinho key={i} id={i} produto={product.item} quantidade="0" valor={product.price} getTotal={this.atualizaTotal} ></ItemCarrinho>
+            })
+          }  
+          <tr>
+            <td colSpan="4">
+              Total = {this.state.totalFinal}
+            </td>
+          </tr>
         </tbody>
       </table>
-      </>
+      
+      </Fragment>
     )
   }
 }  
