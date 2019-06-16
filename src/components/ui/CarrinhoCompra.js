674d944cc4cd623eeb48f86f208bec447a49ed02
Vanessa <vanessayoshida92@gmail.com>
2019-06-16 09:40:34 -0300

    Select funcionando corretamente e funcao para limpar select

--- src/components/ui/CarrinhoCompra.js
+++ src/components/ui/CarrinhoCompra.js
@@ -3,26 +3,26 @@ import ItemCarrinho from './ItemCarrinho';
 
 const menu = {
   "breakfast": [
-      {item:"Café Americano", price: 5},
-      {item:"Café com Leite", price: 7},
-      {item:"Sanduíche de Presunto e Queijo", price: 10},
-      {item:"Suco de Fruta Natural", price: 7}
+      {id:0, item:"Café Americano", price: 5},
+      {id:1, item:"Café com Leite", price: 7},
+      {id:2, item:"Sanduíche de Presunto e Queijo", price: 10},
+      {id:3, item:"Suco de Fruta Natural", price: 7}
   ],
   "lunch": [
-      {item:"Hamburguer Simples Bovino", price:10 },
-      {item:"Hamburguer Simples de Frango", price:10 },
-      {item:"Hamburguer Simples Vegetariano", price:10 },
-      {item:"Hamburguer Duplo Bovino", price:15 },
-      {item:"Hamburguer Duplo Frango", price:15 },
-      {item:"Hamburguer Duplo Vegetariano", price:15 },
-      {item:"Queijo", price:1 },
-      {item:"Ovo", price:1 },
-      {item:"Batata Frita", price:5 },
-      {item:"Anéis de Cebola", price:5 },
-      {item:"Água 500ml", price:5 },
-      {item:"Água 750ml", price:7 },
-      {item:"Bebida gaseificada 500ml", price:7 },
-      {item:"Bebida gaseificada 750ml", price:10 }
+      {id:4, item:"Hamburguer Simples Bovino", price:10 },
+      {id:5, item:"Hamburguer Simples de Frango", price:10 },
+      {id:6, item:"Hamburguer Simples Vegetariano", price:10 },
+      {id:7, item:"Hamburguer Duplo Bovino", price:15 },
+      {id:8, item:"Hamburguer Duplo Frango", price:15 },
+      {id:9, item:"Hamburguer Duplo Vegetariano", price:15 },
+      {id:10, item:"Queijo", price:1 },
+      {id:11, item:"Ovo", price:1 },
+      {id:12, item:"Batata Frita", price:5 },
+      {id:13, item:"Anéis de Cebola", price:5 },
+      {id:14, item:"Água 500ml", price:5 },
+      {id:15, item:"Água 750ml", price:7 },
+      {id:16, item:"Bebida gaseificada 500ml", price:7 },
+      {id:17, item:"Bebida gaseificada 750ml", price:10 }
   ]
 }
 
@@ -30,22 +30,32 @@ class CarrinhoCompra extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      itensTotal: [0, 0, 0, 0],
+      itensTotal: [],
       totalFinal: 0
     }
   }
 
+  handleChange = (event, element) => {
+    const newState = this.state;
+    newState[element] = event.target.value
+    this.setState(newState);
+  }
+
   atualizaTotal = (total, id) => {
-    
-    //pega todos os itens
     let itensTotal = this.state.itensTotal;
-    //atualiza o valor do item atual
     itensTotal[id] = total;
   
     this.setState({
-      //atualiza o total de cada no estado
       itensTotal: itensTotal,
       totalFinal: itensTotal.reduce((ac, item)=> ac + item)
+    })
+  }
+
+  changeCategory = (e) => {
+    this.handleChange(e, "category");
+    this.setState({
+      itensTotal: [],
+      totalFinal: 0
     })
   }
 
@@ -53,6 +63,10 @@ class CarrinhoCompra extends Component {
     const categorySelect = (this.state.category === "lunch") ? "lunch" : "breakfast";
     return (
       <Fragment>
+      <select className="categoryMenu" value=       {this.state.category} onChange={this.changeCategory} >
+          <option value="breakfast" >Café da Manhã</option>
+          <option value="lunch">Almoço ou Janta</option>
+      </select>     
       <table border="1">
         <thead>
         <tr>
@@ -65,7 +79,7 @@ class CarrinhoCompra extends Component {
         <tbody>
           {
             menu[categorySelect].map((product, i) => {
-              return <ItemCarrinho key={i} id={i} produto={product.item} quantidade="0" valor={product.price} getTotal={this.atualizaTotal} ></ItemCarrinho>
+              return <ItemCarrinho key={product.id} id={i} produto={product.item} quantidade="0" valor={product.price} getTotal={this.atualizaTotal} ></ItemCarrinho>
             })
           }  
           <tr>
