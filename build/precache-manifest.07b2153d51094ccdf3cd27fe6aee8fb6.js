58f9911d23b01efec426b147ba5773dbef4e7706
Vanessa <vanessayoshida92@gmail.com>
2019-07-15 08:16:02 -0300

    atualizando o gitignore

---
+++ build/precache-manifest.07b2153d51094ccdf3cd27fe6aee8fb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67220e18693d2d5285cb384aa9d48e6f",
+    "url": "/VanessaYoshida/burger-queen/index.html"
+  },
+  {
+    "revision": "75362527f93176065a5c",
+    "url": "/VanessaYoshida/burger-queen/static/css/main.3cc90cd1.chunk.css"
+  },
+  {
+    "revision": "71281b37c60380e7485a",
+    "url": "/VanessaYoshida/burger-queen/static/js/2.3f108680.chunk.js"
+  },
+  {
+    "revision": "75362527f93176065a5c",
+    "url": "/VanessaYoshida/burger-queen/static/js/main.9ed7e2b4.chunk.js"
+  },
+  {
+    "revision": "e4362e01d1ad8991649e",
+    "url": "/VanessaYoshida/burger-queen/static/js/runtime~main.37c91893.js"
+  },
+  {
+    "revision": "fefd060efd5ab1b183dd0606fda11860",
+    "url": "/VanessaYoshida/burger-queen/static/media/Burger-Queen.fefd060e.png"
+  },
+  {
+    "revision": "2595f137e136b9b3f32113d25bb78262",
+    "url": "/VanessaYoshida/burger-queen/static/media/burger.2595f137.jpg"
+  }
+]);
